cad811a8bbf67a8fd21b9865fb020fe6420610cd
Paulo Khayat <paulokhayat@gmail.com>
2024-07-29 01:19:07 +0300

    Add bunny

--- vbo.py
+++ vbo.py
@@ -83,7 +83,7 @@ class CatVBO(BaseVBO):
         self.attribs = ['in_texcoord_0', 'in_normal', 'in_position']
 
     def get_vertex_data(self):
-        objs = pywavefront.Wavefront('objects/cat/20430_Cat_v1_NEW.obj', cache=True, parse=True)
+        objs = pywavefront.Wavefront('objects/bunny/bunny.obj', cache=True, parse=True)
         obj = objs.materials.popitem()[1]
         vertex_data = obj.vertices
         vertex_data = np.array(vertex_data, dtype='f4')
