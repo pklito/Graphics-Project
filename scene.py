80fe5aac27af9c6e5a07ba1ce32bdc0c69e7167b
Paulo Khayat <paulokhayat@gmail.com>
2024-07-29 03:03:52 +0300

    return old map

--- scene.py
+++ scene.py
@@ -1,6 +1,9 @@
 from model import *
 import glm
+from random import random
 
+def clamp(val, low, high):
+    return max(low, min(high,val))
 
 class Scene:
     def __init__(self, app):
@@ -17,19 +20,18 @@ class Scene:
         app = self.app
         add = self.add_object
 
-        n, s = 2, 1
+        n, s = 3, 1
         height = 0
         for x in range(-n, n, s):
             for z in range(-n, n, s):
-                if(random() < 0.3):
+                if random() < 0.3:
                     height += 1
-                if(random() < 0.3):
+                if random() < 0.3:
                     height -= 1
-                height = max(0, height)
-                height = min(2,height)
-                add(Cube(app, tex_id=int(1.2*random()), pos=(2*x, 2*height - s, 2*z)))
+                height = clamp(height, 0, 3)
+                add(Cube(app, tex_id=int(0.8+1.4*random()),pos=(2*x, 2*height -s, 2*z)))
 
-        #add(Cat(app,rot=(0,0,0), pos=(0, 5, -10),scale=(8,8,8)))
+        add(Cat(app, pos=(0, -2, -10)))
 
     def render(self):
         for obj in self.objects:
