3056d6c6653232b2e27f203a46f2445aa7cfcd47
Paulo Khayat <paulokhayat@gmail.com>
2024-08-02 22:25:03 +0300

    Make camera world oriented

--- camera.py
+++ camera.py
@@ -1,10 +1,10 @@
 import glm
 import pygame as pg
 
-FOV = 50  # deg
+FOV = 80  # deg
 NEAR = 0.1
 FAR = 100
-SPEED = 0.005
+SPEED = 0.008
 SENSITIVITY = 0.04
 
 
@@ -16,6 +16,7 @@ class Camera:
         self.up = glm.vec3(0, 1, 0)
         self.right = glm.vec3(1, 0, 0)
         self.forward = glm.vec3(0, 0, -1)
+        self.forward_level = self.forward #forward without the pitch.
         self.yaw = yaw
         self.pitch = pitch
         # view matrix
@@ -36,6 +37,8 @@ class Camera:
         self.forward.y = glm.sin(pitch)
         self.forward.z = glm.sin(yaw) * glm.cos(pitch)
 
+        self.forward_level = glm.vec3(glm.cos(yaw),0,glm.sin(yaw))
+
         self.forward = glm.normalize(self.forward)
         self.right = glm.normalize(glm.cross(self.forward, glm.vec3(0, 1, 0)))
         self.up = glm.normalize(glm.cross(self.right, self.forward))
@@ -50,17 +53,17 @@ class Camera:
         velocity = SPEED * self.app.delta_time
         keys = pg.key.get_pressed()
         if keys[pg.K_w]:
-            self.position += self.forward * velocity
+            self.position += self.forward_level * velocity
         if keys[pg.K_s]:
-            self.position -= self.forward * velocity
+            self.position -= self.forward_level * velocity
         if keys[pg.K_a]:
             self.position -= self.right * velocity
         if keys[pg.K_d]:
             self.position += self.right * velocity
-        if keys[pg.K_q]:
-            self.position += self.up * velocity
-        if keys[pg.K_e]:
-            self.position -= self.up * velocity
+        if keys[pg.K_SPACE]:
+            self.position += glm.vec3(0,1,0) * velocity
+        if keys[pg.K_LSHIFT]:
+            self.position -= glm.vec3(0,1,0) * velocity
 
     def get_view_matrix(self):
         return glm.lookAt(self.position, self.position + self.forward, self.up)
