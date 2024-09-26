9ad6f44efccd8e450a1485216f4e68c259553f2c
Paulo Khayat <paulokhayat@gmail.com>
2024-09-26 13:10:07 +0300

    Reorder opencv into functions

--- opencv.py
+++ opencv.py
@@ -32,34 +32,47 @@ def drawHoughEdges(overlay, canny):
             x1, y1, x2, y2 = line[0]
             cv.line(overlay, (x1, y1), (x2, y2), (255, 0, 0,255), constants.opencv.HOUGH_PROB_LINE_WIDTH)
 
-def drawHoughBuckets(overlay, canny):
-    if(constants.opencv.HOUGH_LINE_WIDTH <= 0 and not constants.opencv.HOUGH_SHOW_COORDINATES):
-        return
-    lines = cv.HoughLines(canny, 1, np.pi / 180, constants.opencv.HOUGH_THRESH, None, 0, 0)
-    
-    np.set_printoptions(threshold=sys.maxsize)
-    cv.circle(overlay, (600,400), 5, (255,255,255,255))
-    if lines is not None:
-        def toRange(v, min, max, newmin, newmax):
+def _toRange(v, min, max, newmin, newmax):
             if max == min:
                 return (v-min)*(newmax-newmin) + newmin
             return (v - min)*(newmax - newmin)/(max-min)+newmin
-        max_rho, min_rho, max_theta, min_theta = np.sqrt(600*600+400*400), -np.sqrt(600*600+400*400), np.pi, 0
-        for i in range(0, len(lines)):
-            rho = lines[i][0][0]
-            theta = lines[i][0][1]
-            if rho < min_rho or rho > max_rho or theta < min_theta or theta > max_theta:
-                print("out of bounds: ", rho, theta)
-            a = np.cos(theta)
-            b = np.sin(theta)
-            x0 = a * rho
-            y0 = b * rho
-            pt1 = (int(x0 + 1800*(-b)), int(y0 + 1800*(a)))
-            pt2 = (int(x0 - 1800*(-b)), int(y0 - 1800*(a)))
-            if constants.opencv.HOUGH_LINE_WIDTH > 0:
-                cv.line(overlay, pt1, pt2, (0,0,255,50), constants.opencv.HOUGH_LINE_WIDTH, cv.LINE_AA)
-            if constants.opencv.HOUGH_SHOW_COORDINATES:
-                cv.circle(overlay,(int(toRange(theta,min_theta,max_theta,0,600)), int(toRange(rho,min_rho,max_rho,0,400))), 2, (255,255,0,255))
+
+def _polarToLine(rho, theta):
+    """
+    Converts vector to line to two points, which are off screen."""
+    max_rho, min_rho, max_theta, min_theta = np.sqrt(600*600+400*400), -np.sqrt(600*600+400*400), np.pi, 0
+    if rho < min_rho or rho > max_rho or theta < min_theta or theta > max_theta:
+        return None, None
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    pt1 = (int(x0 + 1800*(-b)), int(y0 + 1800*(a)))
+    pt2 = (int(x0 - 1800*(-b)), int(y0 - 1800*(a)))
+    return pt1, pt2
+    
+
+def drawHoughLines(overlay, lines, linewidth = 1):
+    for i in range(0, len(lines)):
+        rho = lines[i][0][0]
+        theta = lines[i][0][1]
+        pt1, pt2 = _polarToLine(rho, theta)
+        if pt1 is None or pt2 is None:
+            continue
+        cv.line(overlay, pt1, pt2, (0,0,255,50), linewidth, cv.LINE_AA)
+
+def drawHoughBuckets(overlay, lines):
+    max_rho, min_rho, max_theta, min_theta = np.sqrt(600*600+400*400), -np.sqrt(600*600+400*400), np.pi, 0
+    if lines is None:
+        return
+    
+    for i in range(0, len(lines)):
+        rho = lines[i][0][0]
+        theta = lines[i][0][1]
+        pt1, pt2 = _polarToLine(rho, theta)
+        if pt1 is None or pt2 is None:
+            continue
+        cv.circle(overlay,(int(_toRange(theta,min_theta,max_theta,0,600)), int(_toRange(rho,min_rho,max_rho,0,400))), 2, (255,255,0,255))
 
 
 fps = 0.0
@@ -93,8 +106,9 @@ def postProcessFbo(app, data_fbo = None):
 
     overlay = np.zeros((canny.shape[0],canny.shape[1],4),dtype=np.uint8)
     if app.SHOW_HOUGH:
-       drawHoughEdges(overlay, canny)
-       drawHoughBuckets(overlay, canny)
+        lines = cv.HoughLines(canny, 1, np.pi / 180, constants.opencv.HOUGH_THRESH, None, 0, 0)
+        drawHoughEdges(overlay, canny)
+        drawHoughBuckets(overlay, lines)
     else:
         overlay = canny
 
@@ -104,8 +118,10 @@ def postProcessImage(file):
     image = cv.imread(file)
     canny = genCannyFromFrameBuffer(image)
     overlay = np.zeros((canny.shape[0],canny.shape[1],4),dtype=np.uint8)
-    drawHoughEdges(overlay, canny)
-    cv.imshow("canny", overlay)
+    #drawHoughEdges(overlay, canny)
+    lines = cv.HoughLines(canny, 1, np.pi / 180, 150, None, 0, 0)
+    drawHoughLines(overlay, lines)
+    cv.imshow("canny", np.hstack((image, overlay[:,:,0:3])))
 
 
 if __name__ == "__main__":
