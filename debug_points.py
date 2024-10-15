f12516c62e32bebe218ff3b7751c909af88da84a
Paulo Khayat <paulokhayat@gmail.com>
2024-10-15 17:48:06 -0400

    Draw squares around boxes

--- debug_points.py
+++ debug_points.py
@@ -15,8 +15,7 @@ import matplotlib.pyplot as plt
 # [2.26, 0.16, 6.08], [2.26, 0.16, 6.08],
 # [-4.55, 0.6, 6.34], [-4.55, 0.6, 6.34]]
 
-trans = [[[2.27, -0.74, -2.03], [1.93, 1.57, 4.52]], [[1.62, -2.14, 0.84], [1.95, 1.75, 4.54]], [[0.94, -2.12, -1.06], [-2.09, -1.58, 6.42]], [[-1.45, 1.53, 0.59], [1.57, -0.47, 5.23]], [[2.1, 1.57, -0.53], [-0.66, -0.27, 6.76]], [[-2.01, 0.21, -0.18], [-1.29, 0.17, 4.3]], [[0.5, 2.89, -0.89], [0.57, 0.33, 5.94]], [[2.04, 0.31, 1.82], [-2.92, 1.89, 4.51]], [[-1.9, -0.8, 1.24], [1.55, -0.46, 5.31]], [[2.08, 1.64, -0.65], [-2.81, 1.93, 4.21]], [[-0.33, -2.87, 1.02], [-3.32, 0.41, 5.76]], [[0.45, 2.78, -0.94], [1.47, -0.39, 4.91]], [[-1.46, 0.99, -1.47], [-4.12, 0.17, 7.12]], [[2.01, 1.57, -0.54], [-2.93, -1.2, 5.9]], [[2.72, -0.0, -0.98], [-3.03, 0.08, 6.95]], [[-1.39, 1.1, -1.73], [-1.42, 0.19, 4.8]]]
-# matrix, _ = cv.Rodrigues(np.array(rvec))
+trans = [[[2.27, -0.74, -2.03], [1.93, 1.57, 4.52]], [[1.62, -2.14, 0.84], [1.95, 1.75, 4.54]], [[0.94, -2.12, -1.06], [-2.09, -1.58, 6.42]], [[-1.45, 1.53, 0.59], [1.57, -0.47, 5.23]], [[2.1, 1.57, -0.53], [-0.66, -0.27, 6.76]], [[-2.01, 0.21, -0.18], [-1.29, 0.17, 4.3]], [[0.5, 2.89, -0.89], [0.57, 0.33, 5.94]], [[2.04, 0.31, 1.82], [-2.92, 1.89, 4.51]], [[-1.9, -0.8, 1.24], [1.55, -0.46, 5.31]], [[2.08, 1.64, -0.65], [-2.81, 1.93, 4.21]], [[-0.33, -2.87, 1.02], [-3.32, 0.41, 5.76]], [[0.45, 2.78, -0.94], [1.47, -0.39, 4.91]], [[-1.46, 0.99, -1.47], [-4.12, 0.17, 7.12]], [[2.01, 1.57, -0.54], [-2.93, -1.2, 5.9]], [[2.72, -0.0, -0.98], [-3.03, 0.08, 6.95]], [[-1.39, 1.1, -1.73], [-1.42, 0.19, 4.8]]]# matrix, _ = cv.Rodrigues(np.array(rvec))
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -33,7 +32,7 @@ rotation_matrix_y = np.array([
 # points = [(matrix.T @ np.array(point)).flatten() for point in points]
 # points = [[p[0] - points[0][0], p[1]- points[0][1], p[2] - points[0][2]] for p in points]
 # points = [[p[0] + 0.35*p[2], p[1], p[2]] for p in points]
-points = [t[1] for t in trans] + [[0,0,0]]
+points = [t[1] for t in trans]
 
 arrows = False
 if arrows:
@@ -47,11 +46,57 @@ y = [point[1] for point in points]
 z = [point[2] for point in points]
 
 ax.scatter(x, y, z, c='r', marker='o')
-# ax.set_xlim([-5, 2])
-# ax.set_ylim([-5, 2])
-# ax.set_zlim([-5, 2])
+
+# Draw render
+ax.scatter([0],[0],[0],c='b')
+screen_points = [[point[0]/point[2], point[1]/point[2],1] for point in points]
+screen_points = np.array(screen_points)
+
+
+# Cubing
+average_fract = [np.average([i - np.floor(i) for i in x]), np.average([i - np.floor(i) for i in y]),np.average([i - np.floor(i) for i in z])]
+print("average fractions:", average_fract)
+# Define the vertices of the cube centered at (1, 2, 1) with side length 2
+
+def draw_cube(ax, point):
+    corner = [np.floor(i) for i in point]
+    corners = [
+    [0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0],
+    [0, 0, 1], [1, 0, 1], [1, 1, 1], [0, 1, 1]
+    ]
+    corners = [np.array(p) + np.array(corner) for p in corners]
+    cube_vertices = np.array(corners)
+
+
+    # Define the edges that connect the vertices
+    cube_edges = [
+        (0, 1), (1, 2), (2, 3), (3, 0),
+        (4, 5), (5, 6), (6, 7), (7, 4),
+        (0, 4), (1, 5), (2, 6), (3, 7)
+    ]
+
+    # Draw the cube
+    for edge in cube_edges:
+        start, end = edge
+        ax.plot(
+            [cube_vertices[start][0], cube_vertices[end][0]],
+            [cube_vertices[start][1], cube_vertices[end][1]],
+            [cube_vertices[start][2], cube_vertices[end][2]],
+            color='k'
+        )
+
+for p in points:
+    draw_cube(ax,p)
+
+# Set the aspect ratio to be equal
+ax.set_box_aspect([1, 1, 1])
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
+
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+ax2.scatter(screen_points[:,0],screen_points[:,1], c='g')
 
 plt.show()
