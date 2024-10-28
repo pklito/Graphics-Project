Fix bug in average matrix calculating

main.py
@@ -126,8 +126,9 @@ class GraphicsEngine:
                 # for a1,a2,a3 in itertools.product([-1, 1], repeat=3):
                     
                 #     newcubes = [[a1*b[0], a2*b[1], a3*b[2]] for b in cubes]
+                print("new cubes: " + str(newcubes))
                 for c in newcubes:
-                    self.scene.add_object(MarkerCube(self, pos=[np.floor(x1+x2) for x1,x2 in zip(c,self.camera.position)]))
+                    self.scene.add_object(MarkerCube(self, pos=[x1 for x1,x2 in zip(c,self.camera.position)]))
         pg.display.flip()
 
     def render_pipeline(self):

opencv_points.py
@@ -52,13 +52,13 @@ def transToCubes(trans, threshold = 0.97):
     y_temp = average_mat[:,1]/np.linalg.norm(average_mat[:,1])
     average_mat = np.array([x_temp, y_temp, np.cross(x_temp, y_temp)]).T
     for i in range(len(trans)):
-        mats = [(sorted(get_options(cv.Rodrigues(np.array(t[0]))[0]),key=get_comp(np.eye(3)))[-1], t[1]) for t in trans]
         
         mat_size = len(mats)
         mats = [(m[0], m[1]) for m in mats if get_comp(average_mat)(m[0]) > threshold]
         if len(mats) > 0.5 * mat_size:
             break
         print("Failed to find good average_matrix, ", len(mats) / mat_size)
+        mats = [(sorted(get_options(cv.Rodrigues(np.array(t[0]))[0]),key=get_comp(np.eye(3)))[-1], t[1]) for t in trans]
         average_mat = mats[np.random.randint(0,mat_size)][0]
 
     if len(mats) == 0: