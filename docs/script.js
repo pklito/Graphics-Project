f5f7bf57fc00a72767668fc924a56014c2fa4532
Paulo Khayat <paulokhayat@gmail.com>
2024-12-15 11:04:27 +0200

    Start page from scratch with albert's examples

--- docs/script.js
+++
@@ -1,6 +0,0 @@
-fetch('README.md')
-    .then(response => response.text())
-    .then(text => {
-        document.getElementById('content').innerHTML = marked(text);
-    })
-    .catch(error => console.error('Error fetching README.md:', error));
